526b633b5253428e04098239cf9be7007c167085
monika427 <monikachapagain01@gmail.com>
2023-05-02 10:58:26 +0545

    fixed button error

--- src/3TGame/Board.jsx
+++ src/3TGame/Board.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Square from './Square';
 
+
 const Board = () => 
 {
     const [state, setState] = useState(Array(9).fill(null));
@@ -39,25 +40,25 @@ const handleReset =() =>{
 };
 console.log("State", state);
 return (
-    <div className="board-container">
+    <div className="Board-container">
             {iswinner ?(
-                <>{iswinner} won the game<button onclick= {handleReset}>Play Again</button></>
+                <>{iswinner} won the game<button onClick= {handleReset}>Play Again</button></>
             ) : (
     <>
     <h4>player{isXTurn ? "X" : "0"} please move</h4>
-        <div className="board-row">
+        <div className="Board-row">
             <Square onClick={() => handleClick(0)} value={state[0]} />
             <Square onClick={() => handleClick(1)} value={state[1]} />
             <Square onClick={() => handleClick(2)} value={state[2]} />
         </div>
-        <div className='board-row'>
+        <div className='Board-row'>
             <Square onClick={() => handleClick(3)} value={state[3]} />
             <Square onClick={() => handleClick(4)} value={state[4]} />
             <Square onClick={() => handleClick(5)} value={state[5]} />
         </div>
 
 
-        <div className='board-row'>
+        <div className='Board-row'>
             <Square onClick={() => handleClick(6)} value={state[6]} />
             <Square onClick={() => handleClick(7)} value={state[7]} />
             <Square onClick={() => handleClick(8)} value={state[8]} />
